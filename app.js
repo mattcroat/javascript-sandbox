06e76734aa6a13c106af0e260609de18d464f6ea
mattcroat <mattcroat.dev@gmail.com>
2020-12-07 20:57:12 +0100

    feat: :sparkles: Add JSX output

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ function debounce(callback, delay) {
 
 function setIframeContent() {
   const js = textareaEl.value.trim()
+  const code = JSON.stringify(js)
 
   const source = `
     <html>
@@ -26,31 +27,55 @@ function setIframeContent() {
         }
 
         body {
-          width: 100vw;
           font-family: 'Inter', sans-serif;
           font-size: 1.25rem;
           color: snow;
-          padding: 2rem;
         }
 
-        h1, p {
+        #app {
+          padding: 2rem;
+          margin-bottom: 2rem;
+        }
+
+        h1, h2, p {
           margin: 1rem 0;
+        }
+
+        #output {
+          font-size: 1rem;
+          padding: 0 2rem 0 2rem;
+          border-top: 1px solid hsl(220, 20%, 10%);
+          line-height: 1.6;
+        }
+
+        pre {
+          white-space: pre-wrap;
+          word-wrap: break-word;
         }
       </style>
     </head>
     <body>
       <div id="app"></div>
 
-      <!-- TODO: change to production -->
+      <div id="output"></div>
+
       <script src="https://unpkg.com/react@17/umd/react.development.js"><\/script>
       <script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js"><\/script>
       <script src="https://unpkg.com/@babel/standalone/babel.min.js"><\/script>
       <script type="text/babel">
         ${js}
+      <\/script>
+      <script>
+        const options = { presets: ['react'] }
+        const { code } = Babel.transform(${code}, options);
 
-        const banana = 'const App = () => <h1>Hello, World!</h1>'
-        const output = Babel.transform(input, { presets: ['env', 'react'] }).code
-        console.log(output)
+        const titleEl = document.createElement('h2')
+        titleEl.innerText = 'JSX Output'
+
+        const targetEl = document.getElementById('output')
+        targetEl.innerHTML = '<pre>' + code.replace(/\\/\\*#__PURE__\\*\\//g, '') + '</pre>'
+
+        targetEl.prepend(titleEl)
       <\/script>
     </body>
     </html>
