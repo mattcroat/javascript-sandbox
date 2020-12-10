8aba8d33e6ae89ad083e5dd8092221c2e5d0b288
mattcroat <mattcroat.dev@gmail.com>
2020-12-10 21:39:17 +0100

    style: :lipstick: Add Google fonts, CSS variables, fixes

--- app.js
+++ app.js
@@ -41,6 +41,8 @@ function setIframeContent(code) {
     <html>
     <head>
       <style>
+        @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
+
         body {
           font-family: 'Inter', sans-serif;
           overflow: hidden;
